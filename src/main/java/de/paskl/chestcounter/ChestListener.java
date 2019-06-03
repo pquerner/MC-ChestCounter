df878357d574a3b4485f6d65256edc8dcd6635ed
Pascal <pq@paskl.de>
2019-06-03 22:20:46 +0200

    change: extract some stuff to constants change: remove duplicate code change: chest-signs can now be of any wall sign type, EXCEPT birch sign -> birch wall sign is exclusively set to the "total" counter (might change it too by using a different keyword than [Counter] add: doc code a bit

--- src/main/java/de/paskl/chestcounter/ChestListener.java
+++ src/main/java/de/paskl/chestcounter/ChestListener.java
@@ -18,6 +18,8 @@ import java.util.*;
 public class ChestListener implements Listener {
 
     public static final String COUNTER_LINE = "[Counter]";
+    public static final int BLOCK_LOOKUP_DOWN = 3;
+    public static final Material COUNT_CHILDREN_SIGN_MATERIAL = Material.BIRCH_WALL_SIGN;
     //Look at normal and trapped chests (also works on Doublechests or either)
     List<Material> chestsMaterialList = new ArrayList<Material>(Arrays.asList(Material.CHEST, Material.TRAPPED_CHEST));
     private ChestCounter plugin;
@@ -32,83 +34,79 @@ public class ChestListener implements Listener {
         if (ev.getAction() == Action.RIGHT_CLICK_BLOCK) {
             if (block.getType().toString().endsWith("WALL_SIGN")) {
                 Sign sign = (Sign) ev.getClickedBlock().getState();
-                if (block.getType() != Material.ACACIA_WALL_SIGN) {
-                    if (sign.getLine(0).equals(COUNTER_LINE)) {
-                        org.bukkit.block.data.type.WallSign data = (org.bukkit.block.data.type.WallSign) sign.getBlockData();
-                        BlockFace face = null;
-                        //Must invert, because I cannot click sign "from behind" - its always facing towards the player
-                        face = getBlockFace(data, face);
+                org.bukkit.block.data.type.WallSign data = (org.bukkit.block.data.type.WallSign) sign.getBlockData();
+                BlockFace face = null;
+                if (!sign.getLine(0).equals(COUNTER_LINE)) {
+                    return;
+                }
+                if (block.getType() != COUNT_CHILDREN_SIGN_MATERIAL) { //Is every other sign
 
-                        if (face != null) {
-                            Block blockRelative = block.getRelative(face);
-                            if (chestsMaterialList.contains(blockRelative.getState().getBlock().getType())) {
-                                Chest chest = (Chest) blockRelative.getState();
-                                int amount;
-                                int amountMax;
-                                boolean addedMaxAmount;
-                                amountMax = 0;
-                                amount = 0;
-                                Inventory inventory = chest.getInventory();
+                    //Must invert, because I cannot click sign "from behind" - its always facing towards the player
+                    face = getBlockFace(data, face);
 
-                                if (inventory instanceof DoubleChestInventory) {
-                                    DoubleChest doubleChest = (DoubleChest) inventory.getHolder();
-                                    inventory = doubleChest.getInventory();
-                                }
+                    if (face != null) {
+                        Block blockRelative = block.getRelative(face);
+                        if (chestsMaterialList.contains(blockRelative.getState().getBlock().getType())) {
+                            Chest chest = (Chest) blockRelative.getState();
+                            int amount;
+                            int amountMax;
+                            boolean addedMaxAmount;
+                            amountMax = 0;
+                            amount = 0;
+                            Inventory inventory = chest.getInventory();
 
-                                if (sign != null) { //Only work if sign is available
-                                    //Check if sign has metadata attached. Should just fetch that data before and then update if needed, ie
-                                    //add tstamp to metadata field and then only check once every 5 minutes?
-                                    addedMaxAmount = false;
-                                    for (ItemStack item : inventory.getContents().clone()) {
-                                        if (item == null) continue; //Will also check for AIR
-                                        amount += item.getAmount();
-                                        if (inventory.getSize() > 0 && !addedMaxAmount) { //Only calc Y once
-                                            amountMax += inventory.getSize() * item.getMaxStackSize();
-                                        }
-                                        addedMaxAmount = true;
-                                    }
-
-                                    sign.setLine(1, String.valueOf(amount) + " / " + String.valueOf(amountMax));
-                                    sign.setMetadata("loc", new FixedMetadataValue(plugin, String.valueOf(sign.getLocation().getBlockX()) + ";"
-                                            + String.valueOf(sign.getLocation().getBlockY())
-                                            + ";" + String.valueOf(sign.getLocation().getBlockZ())));
-                                    sign.update(true);
-                                }
-                            } else {
-                                ev.getPlayer().sendMessage(ChatColor.RED + "Sign must be attached to a chest!");
-                            }
-                        }
-                    }
-                } else {
-                    if (sign.getLine(0).equals(COUNTER_LINE)) {
-                        org.bukkit.block.data.type.WallSign data = (org.bukkit.block.data.type.WallSign) sign.getBlockData();
-                        BlockFace face = null;
-
-                        List<Block> nextblocksY = getBlocks(ev.getClickedBlock(), 3);
-
-                        //Must invert, because I cannot click sign "from behind" - its always facing towards the player
-                        face = getBlockFace(data, face);
-
-                        if (face != null && !nextblocksY.isEmpty()) {
-                            int currentTotalAmount = 0;
-                            int maxTotalAmount = 0;
-                            for (Block b : nextblocksY) {
-                                Sign childSign = (Sign) b.getState();
-
-                                String[] exploded = childSign.getLine(1).split("/");
-                                Integer currentAmount = Integer.parseInt(exploded[0].trim());
-                                Integer chestMaximumAmount = Integer.parseInt(exploded[1].trim());
-
-                                currentTotalAmount += currentAmount;
-                                maxTotalAmount += chestMaximumAmount;
-
-
+                            if (inventory instanceof DoubleChestInventory) {
+                                DoubleChest doubleChest = (DoubleChest) inventory.getHolder();
+                                inventory = doubleChest.getInventory();
                             }
 
-                            sign.setLine(1, String.valueOf(currentTotalAmount) + " / " + String.valueOf(maxTotalAmount));
+
+                            addedMaxAmount = false;
+                            for (ItemStack item : inventory.getContents().clone()) {
+                                if (item == null) continue; //Will also check for AIR
+                                amount += item.getAmount();
+                                if (inventory.getSize() > 0 && !addedMaxAmount) { //Only calc Y once
+                                    amountMax += inventory.getSize() * item.getMaxStackSize();
+                                }
+                                addedMaxAmount = true;
+                            }
+
+                            sign.setLine(1, String.valueOf(amount) + " / " + String.valueOf(amountMax));
                             sign.update(true);
+                        } else {
+                            ev.getPlayer().sendMessage(ChatColor.RED + "Sign must be attached to a chest!");
                         }
                     }
+                } else { //Is "count children sign"
+
+                    List<Block> nextblocksY = getBlocks(ev.getClickedBlock(), BLOCK_LOOKUP_DOWN);
+
+                    if (nextblocksY.isEmpty()) {
+                        ev.getPlayer().sendMessage(ChatColor.RED + "No children chests found.");
+                        return;
+                    }
+
+                    //Must invert, because I cannot click sign "from behind" - its always facing towards the player
+                    face = getBlockFace(data, face);
+
+                    if (face != null && !nextblocksY.isEmpty()) {
+                        int currentTotalAmount = 0;
+                        int maxTotalAmount = 0;
+                        for (Block b : nextblocksY) {
+                            Sign childSign = (Sign) b.getState();
+
+                            String[] exploded = childSign.getLine(1).split("/");
+                            Integer currentAmount = Integer.parseInt(exploded[0].trim());
+                            Integer chestMaximumAmount = Integer.parseInt(exploded[1].trim());
+
+                            currentTotalAmount += currentAmount;
+                            maxTotalAmount += chestMaximumAmount;
+                        }
+
+                        sign.setLine(1, String.valueOf(currentTotalAmount) + " / " + String.valueOf(maxTotalAmount));
+                        sign.update(true);
+                    }
+
                 }
             }
         }
@@ -120,7 +118,8 @@ public class ChestListener implements Listener {
         int i = start.getY() + howManyBlocksDown;
         while (i > 0) {
             Block b = start.getWorld().getBlockAt(start.getX(), i, start.getZ());
-            if (b.getType() == Material.DARK_OAK_WALL_SIGN) {
+            if (b.getType().toString().endsWith("_WALL_SIGN") &&
+                    !(b.getType() == Material.BIRCH_WALL_SIGN)) {
                 Sign s = (Sign) b.getState();
                 if (s.getLine(0).equals(COUNTER_LINE)) {
                     blocks.add(b);
