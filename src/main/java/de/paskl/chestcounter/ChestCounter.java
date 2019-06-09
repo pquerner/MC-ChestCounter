157d512de2fffe49abb34095d569840e075d61da
Pascal <pq@paskl.de>
2019-06-09 04:16:56 +0200

    add: add message about automatic update

--- src/main/java/de/paskl/chestcounter/ChestCounter.java
+++ src/main/java/de/paskl/chestcounter/ChestCounter.java
@@ -1,6 +1,7 @@
 package de.paskl.chestcounter;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChestCounter extends JavaPlugin {
@@ -10,7 +11,9 @@ public class ChestCounter extends JavaPlugin {
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
             public void run() {
                 CommandUpdate cu = new CommandUpdate(getPlugin(ChestCounter.class));
+                getPlugin(ChestCounter.class).getLogger().info(ChatColor.BLUE + "Begin updating signs.");
                 cu.updateSigns();
+                getPlugin(ChestCounter.class).getLogger().info(ChatColor.BLUE + "Updated signs.");
             }
         }, 0L, 20L * 300); //Every 5 min
         this.getCommand("signupdater").setExecutor(new CommandUpdate(this));
